cd286a50e755f8eab8974d754f9f0b6af26ecdcc
TomilovVA2003 <104489471+TomilovVA2003@users.noreply.github.com>
2025-12-11 11:01:30 +0300

    gitignore fix / include

---
+++ bitrix-docker/www/include/page_index/block_brands_slider.php
@@ -0,0 +1,121 @@
+<div class="related-list">
+    <h2>Бренды</h2>
+    <div class="swiper-container related-slider" style=" max-height:180px;margin:0px;padding-top:15px;padding-bottom:15px; border-bottom: 1px dotted #caced3;border-top: 1px dotted #caced3;">
+        <div class="swiper-wrapper"   >
+            <div class="swiper-slide"  >
+                <div class="product-item_img" style=" max-height:150px;margin:0px;">
+                    <a href="https://eklektika.ru/yoliba/" class="client" target="_blank"><img src="<?=SITE_TEMPLATE_PATH?>/assets/img/slider_brands/yolibalogo.jpg" alt=""></a>
+                </div>
+            </div>
+            <div class="swiper-slide"  >
+                <div class="product-item_img" style=" max-height:150px;margin:0px;">
+                    <a href="/prestigio/" class="client" target="_blank"><img src="<?=SITE_TEMPLATE_PATH?>/assets/img/slider_brands/logo_site_new.png" alt=""></a>
+                </div> 
+            </div>
+            <div class="swiper-slide"  >
+                <div class="product-item_img" style=" max-height:150px;margin:0px;">
+                    <img src="<?=SITE_TEMPLATE_PATH?>/assets/img/slider_brands/akcii/%D0%BA%D0%BA.gif" alt="">
+                </div> 
+            </div>
+            <div class="swiper-slide"  >
+                <div class="product-item_img" style=" max-height:150px;margin:0px;">
+                    <a href="/xindao/" class="client" target="_blank"><img src="<?=SITE_TEMPLATE_PATH?>/assets/img/slider_brands/xindao.jpg" alt=""></a>
+                </div>
+            </div>
+            <div class="swiper-slide"  >
+                <div class="product-item_img" style=" max-height:150px;margin:0px;">
+                    <a href="https://eklektika.ru/wenger/" class="client" target="_blank"><img src="<?=SITE_TEMPLATE_PATH?>/assets/img/slider_brands/wenger.jpg" alt=""></a>
+                </div>
+            </div>
+            <div class="swiper-slide"  >
+                <div class="product-item_img" style=" max-height:150px;margin:0px;">
+                    <a href="https://eklektika.ru/cross/" class="client" target="_blank"><img src="<?=SITE_TEMPLATE_PATH?>/assets/img/slider_brands/cross.jpg" alt=""></a>
+                </div>
+            </div>  		 
+            <div class="swiper-slide"  >
+                <div class="product-item_img" style=" max-height:150px;margin:0px;">
+                    <a href="https://eklektika.ru/brendy/brunovisconti/" class="client" target="_blank"><img src="<?=SITE_TEMPLATE_PATH?>/assets/img/slider_brands/brunovisconti.jpg" alt=""></a>
+                </div>
+            </div>
+            <div class="swiper-slide"  >
+                <div class="product-item_img" style=" max-height:150px;margin:0px;">
+                    <img src="<?=SITE_TEMPLATE_PATH?>/assets/img/slider_brands/Parker.jpg" alt="">
+                </div>
+            </div>
+            <div class="swiper-slide"  >
+                <div class="product-item_img" style=" max-height:150px;margin:0px;" >
+                    <a href="/enote/" class="client" target="_blank"><img src="<?=SITE_TEMPLATE_PATH?>/assets/img/slider_brands/enot.png" alt=""></a>
+                </div>
+            </div>
+            <div class="swiper-slide"  >
+                <div class="product-item_img" style=" max-height:150px;margin:0px;">
+                    <a href="/lettertone/" class="client" target="_blank"><img src="<?=SITE_TEMPLATE_PATH?>/assets/img/slider_brands/lettertone.jpg" alt=""></a>
+                </div>
+            </div>
+            <div class="swiper-slide"  >
+                <div class="product-item_img" style=" max-height:150px;margin:0px;">
+                    <img src="<?=SITE_TEMPLATE_PATH?>/assets/img/slider_brands/Waterman.jpg" alt="">
+                </div>
+            </div>
+            <div class="swiper-slide"  >
+                <div class="product-item_img" style=" max-height:150px;margin:0px;">
+                    <a href="/senator/" class="client" target="_blank"><img src="<?=SITE_TEMPLATE_PATH?>/assets/img/slider_brands/senator.jpg" alt=""></a>
+                </div>
+            </div>
+            <div class="swiper-slide"  >
+                <div class="product-item_img" style=" max-height:150px;margin:0px;">
+                    <a href="/prodir/" class="client" target="_blank"><img src="<?=SITE_TEMPLATE_PATH?>/assets/img/slider_brands/prodir.jpg" alt=""></a>
+                </div>
+            </div>
+            <div class="swiper-slide"  >
+                <div class="product-item_img" style=" max-height:150px;margin:0px;">
+                    <a href="/philippi/" class="client" target="_blank"><img src="<?=SITE_TEMPLATE_PATH?>/assets/img/slider_brands/filippi.jpg" alt=""></a>
+                </div>
+            </div>
+            <div class="swiper-slide"  >
+                <div class="product-item_img" style=" max-height:150px;margin:0px;">
+                    <a href="/sols/" class="client" target="_blank"><img src="<?=SITE_TEMPLATE_PATH?>/assets/img/slider_brands/sols.jpg" alt=""></a>
+                </div>
+            </div>
+            <div class="swiper-slide"  >
+                <div class="product-item_img" style=" max-height:150px;margin:0px;">
+                    <a href="/altavolo/" class="client" target="_blank"><img src="<?=SITE_TEMPLATE_PATH?>/assets/img/slider_brands/altavolo.jpg" alt=""></a>
+                </div>
+            </div>
+            <div class="swiper-slide"  >
+                <div class="product-item_img" style=" max-height:150px;margin:0px;">
+                    <a href="/mister-christmas/" class="client" target="_blank"><img src="<?=SITE_TEMPLATE_PATH?>/assets/img/slider_brands/mscris.jpg" alt=""></a>
+                </div>
+            </div>
+            <div class="swiper-slide"  >
+                <div class="product-item_img" style=" max-height:150px;margin:0px;">
+                    <a href="https://eklektika.ru/victorinox/" class="client" target="_blank"><img src="<?=SITE_TEMPLATE_PATH?>/assets/img/slider_brands/victorinox.jpg" alt=""></a>
+                </div>
+            </div>
+            <div class="swiper-slide"  >
+                <div class="product-item_img" style=" max-height:150px;margin:0px;">
+                    <a href="/open/" class="client" target="_blank"><img src="<?=SITE_TEMPLATE_PATH?>/assets/img/slider_brands/open.jpg" alt=""></a>
+                </div>
+            </div>
+            <div class="swiper-slide"  >
+                <div class="product-item_img" style=" max-height:150px;margin:0px;">
+                    <a href="/mid-ocean/" class="client" target="_blank"><img src="<?=SITE_TEMPLATE_PATH?>/assets/img/slider_brands/midoceanLogo.jpg" alt=""></a>
+                </div>
+            </div>
+            <div class="swiper-slide"  >
+                <div class="product-item_img" style=" max-height:150px;margin:0px;">
+                    <a href="https://eklektika.ru/pierre-cardin/" class="client" target="_blank"><img src="assets/images/ev_cat/pierre-cardin.jpg" alt=""></a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="swiper-nav cp-nav" style=" max-height:150px;padding-top:5px;margin-top:5px;">
+        <div class="cp-button-prev btn-prev-0 swiper-button-disabled" tabindex="0" role="button" aria-label="Previous slide" aria-disabled="true">
+            <i class="icon-arrow"></i>
+        </div>
+        <div class="cp-button-next btn-next-0" tabindex="0" role="button" aria-label="Next slide" aria-disabled="false">
+            <i class="icon-arrow"></i>
+        </div>
+    </div>
+</div> 
---
+++ bitrix-docker/www/include/page_index/block_service.php
@@ -0,0 +1,60 @@
+<div class="row no-gutters flex-wrapper main-items-anons">
+    <div class="item-anons">
+        <a href="#" class="item" style="background: linear-gradient(99.89deg, #F4F4F4 1.16%, #F6FAFF 101.52%)">
+            <img data-src="<?=SITE_TEMPLATE_PATH?>/assets/img/anons/lazernaya-gravirovka.png" alt="" src="<?=SITE_TEMPLATE_PATH?>/assets/img/anons/lazernaya-gravirovka.png" class="lazy-loaded">
+            <span class="item-top">
+                <span class="item-title"><p class="h3 strong">Лазерная гравировка сувениров</p></span>
+                <span class="item-description"><p>Лазерную гравировку можно наносить на сувенирную продукцию из&nbsp;металла, стекла, хрусталя, пластика, дерева, кожи, вспененной резины.</p></span>
+            </span>
+        </a>
+    </div>
+    <div class="item-anons">
+        <a href="#" class="item" style="background: linear-gradient(99.89deg, #F4F4F4 1.16%, #F6FAFF 101.52%)">
+            <img data-src="<?=SITE_TEMPLATE_PATH?>/assets/img/anons/tisnenie.png" alt="" src="<?=SITE_TEMPLATE_PATH?>/assets/img/anons/tisnenie.png" class="lazy-loaded">
+            <span class="item-top">
+                <span class="item-title"><p class="h3 strong">Горячее тиснение и тиснение фольгой на сувенирах и подарках</p></span>
+                <span class="item-description"><p>Подходит для продукции из натуральной, эко-кожи, дерева, бумаги, картона.</p></span>
+            </span>
+        </a>
+    </div>
+</div>
+<div class="row no-gutters flex-wrapper main-items-anons">
+    <div class="item-anons">
+        <a href="#" class="item" style="background: linear-gradient(99.89deg, #F4F4F4 1.16%, #F6FAFF 101.52%)">
+            <img data-src="<?=SITE_TEMPLATE_PATH?>/assets/img/anons/polnocvetnaya-uf-pechat.png" alt="" src="<?=SITE_TEMPLATE_PATH?>/assets/img/anons/polnocvetnaya-uf-pechat.png" class="lazy-loaded">
+            <span class="item-top">
+                <span class="item-title"><p class="h3 strong">Полноцветная УФ-печать</p></span>
+                <span class="item-description"><p>Для сувениров из любых материалов: стекло, дерево, пластик, металл, бумага, кожа, ткань</p></span>
+            </span>
+        </a>
+    </div>
+    <div class="item-anons">
+        <a href="#" class="item" style="background: linear-gradient(99.89deg, #F4F4F4 1.16%, #F6FAFF 101.52%)">
+            <img data-src="<?=SITE_TEMPLATE_PATH?>/assets/img/anons/tampopechat.png" alt="" src="<?=SITE_TEMPLATE_PATH?>/assets/img/anons/tampopechat.png" class="lazy-loaded">
+            <span class="item-top">
+                <span class="item-title"><p class="h3 strong">Тампопечать</p></span>
+                <span class="item-description"><p>Для деликатной сувенирной продукции из кожи, бумаги, пластика, металла, дерева, керамики, стекла</p></span>
+            </span>
+        </a>
+    </div>
+</div>
+<div class="row no-gutters flex-wrapper main-items-anons">
+    <div class="item-anons">
+        <a href="#" class="item" style="background: linear-gradient(99.89deg, #F4F4F4 1.16%, #F6FAFF 101.52%)">
+            <img data-src="<?=SITE_TEMPLATE_PATH?>/assets/img/anons/shelkografiya.png" alt="" src="<?=SITE_TEMPLATE_PATH?>/assets/img/anons/shelkografiya.png" class="lazy-loaded">
+            <span class="item-top">
+                <span class="item-title"><p class="h3 strong">Шелкография</p></span>
+                <span class="item-description"><p>Перенос логотипов компании, изображений и надписей алкидными и другими устойчивыми синтетическими красками</p></span>
+            </span>
+        </a>
+    </div>
+    <div class="item-anons">
+        <a href="#" class="item" style="background: linear-gradient(99.89deg, #F4F4F4 1.16%, #F6FAFF 101.52%)">
+            <img data-src="<?=SITE_TEMPLATE_PATH?>/assets/img/anons/Ellipse.png" alt="" src="<?=SITE_TEMPLATE_PATH?>/assets/img/anons/Ellipse.png" class="lazy-loaded">
+            <span class="item-top">
+                <span class="item-title"><p class="h3 strong">DTF печать</p></span>
+                <span class="item-description"><p>Нанесение высококачественных изображений на различные типы одежды, включая футболки, кепки и рюкзаки. Этот метод печати обладает рядом преимуществ, таких как высокая производительность, автоматизация и возможность изготовления продукции в любых объёмах.</p></span>
+            </span>
+        </a>
+    </div>
+</div>
